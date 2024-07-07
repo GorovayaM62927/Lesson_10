337dc30009f38095f7b9bf032afe3c5715f48c12
gorovaya <mill-a84@mail.ru>
2024-07-07 22:58:38 +0300

    git3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.math.BigInteger;
+
 public class Main {
+    public static BigInteger getFactorial(int f) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= f; i++)
+            result = result.multiply(BigInteger.valueOf(i));
+        return result;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(getFactorial(100));
     }
 }
